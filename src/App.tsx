e7b2e31b2e01ca915f9289c6d34e98240cf96872
trevorkerney <trevorkerney@hotmail.com>
2026-03-31 20:22:35 -0500

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="flex h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold">waverunner</h1>
+    </main>
+  );
+}
+
+export default App;
